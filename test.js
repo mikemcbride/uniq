eeec37cfe24029029757d82b208ca8a193afe7e5
Mike McBride <mike.mcbride@hey.com>
2021-06-24 22:05:45 -0500

    jest => uvu

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const { test } = require('uvu')
+const assert = require('uvu/assert')
 const uniq = require('./index.js')
 
 // setup
@@ -5,12 +7,16 @@ const arrayWithDuplicates = ['cat', 'dog', 'pig', 'cat', 'monkey', 'pig']
 
 // tests
 test('given an array, it returns a new array without duplicate items', () => {
-  expect(uniq(arrayWithDuplicates).length).toEqual(4)
+    const expected = 4
+    const value = uniq(arrayWithDuplicates)
+    assert.equal(value.length, expected)
 })
 
 test('returns a tuple of unique array and removed items when passing optional parameter', () => {
   let [deduped, dupes] = uniq(arrayWithDuplicates, { includeDuplicates: true })
-  expect(deduped.length).toEqual(4)
-  expect(dupes.length).toEqual(2)
-  expect(dupes.includes('cat')).toBe(true)
+  assert.equal(deduped.length, 4)
+  assert.equal(dupes.length, 2)
+  assert.equal(dupes.includes('cat'), true)
 })
+
+test.run()
